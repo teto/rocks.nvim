6fe8b27e533850a198b2ae952f539c0b767f1e14
Matthieu C. <886074+teto@users.noreply.github.com>
2024-06-29 00:04:40 +0200

    feat: make errors more visible with vim.notify
    the spinner is too discrete and there is a green tick once it finishes so at first I thought the install had succeeded but paying closer attention, the message up/down of it said install had failed

--- lua/rocks/operations/init.lua
+++ lua/rocks/operations/init.lua
@@ -305,6 +305,7 @@ operations.sync = function(user_rocks, on_complete)
             if not vim.tbl_isempty(error_handles) then
                 local message = "Sync completed with errors! Run ':Rocks log' for details."
                 log.error(message)
+                vim.notify(message, vim.log.levels.ERROR)
                 progress_handle:report({
                     title = "Error",
                     message = message,
@@ -555,6 +556,7 @@ operations.add = function(arg_list, callback)
                     title = "Installation failed",
                     message = message,
                 })
+                vim.notify(message, vim.log.levels.ERROR)
                 if not_found then
                     prompt_retry_install_with_dev(arg_list, rock_name, version)
                 end
